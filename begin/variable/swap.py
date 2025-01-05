c88ca04e3e29d030722bfeb68d4c9f528c5a15ec
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-05 17:52:54 +0300

    Add new file swap.py

---
+++ begin/variable/swap.py
@@ -0,0 +1,19 @@
+# swap Java
+# int var1 = 3;
+# int var2 = 5;
+#
+# int tmp = var1;
+# var1 = var2;
+# var2 = tmp
+
+# swap Python
+var1 = 3
+var2 = 5
+
+print("var1: " + var1)
+print("var2: " + var2)
+
+var2, var1 = var1, var2
+
+print("var1: " + var1)
+print("var2: " + var2)
